dfa3e06e1b87e22b598a94814c4bae51ae711105
paulm130 <158609527+paulm130@users.noreply.github.com>
2026-05-26 21:54:26 -0700

    implemented expose and service workers part of explore

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,6 +45,12 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
+  if("serviceWorker" in navigator) {
+    console.log("browser supports serviceWorker");
+  }
+  else {
+    console.log("browser does not support serviceWorker");
+  }
   // B2. TODO - Listen for the 'load' event on the window object.
   // Steps B3-B6 will be *inside* the event listener's function created in B2
   // B3. TODO - Register './sw.js' as a service worker (The MDN article
@@ -54,6 +60,15 @@ function initializeServiceWorker() {
   // B5. TODO - In the event that the service worker registration fails, console
   //            log that it has failed.
   // STEPS B6 ONWARDS WILL BE IN /sw.js
+  window.addEventListener("load", async (event) => {
+    try {
+      const registration = await navigator.serviceWorker.register('./sw.js');
+      console.log("success");
+    } 
+    catch(e) {
+      console.log("failure" + e);
+    }
+  });
 }
 
 /**
@@ -71,7 +86,13 @@ async function getRecipes() {
   /**************************/
   // The rest of this method will be concerned with requesting the recipes
   // from the network
+  let recipes = localStorage.getItem('recipes');
+	if (recipes) 
+	{
+		return JSON.parse(recipes);
+	} 
   // A2. TODO - Create an empty array to hold the recipes that you will fetch
+  let newRecipes = [];
   // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
   //            has a great article on them. A promise takes one parameter - A
   //            function (we call these callback functions). That function will
@@ -100,6 +121,22 @@ async function getRecipes() {
   //            resolve() method.
   // A10. TODO - Log any errors from catch using console.error
   // A11. TODO - Pass any errors to the Promise's reject() function
+  return new Promise(async (resolve,reject) => {
+    let newRecipes = [];
+    for(let i=0;i<RECIPE_URLS.length;i++) {
+      try{
+        let url = await fetch(RECIPE_URLS[i]);
+        let json = await url.json();
+        newRecipes.push(json);
+      }
+      catch(error) {
+        console.error(error);
+        reject(error);
+      }
+    }
+    saveRecipesToStorage(newRecipes);
+    resolve(newRecipes);
+  });
 }
 
 /**
